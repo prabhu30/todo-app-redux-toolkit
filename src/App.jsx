1e08b665fbdb9fafd419f76833f5b919323aa1d6
Prabhu Kalyan Korivi <prabhukalyan30@gmail.com>
2024-04-15 11:45:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='font-bold text-2xl text-black mt-12'>Todo List using Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,35 @@
+import { useDispatch } from 'react-redux'
+import { addTodo } from '../store/slices/todoSlice';
+import { useState } from 'react';
+
+const AddTodo = () => {
+
+    const [ input, setInput ] = useState('');
+    const dispatch = useDispatch();
+
+    function addTodoHandler(event) {
+        event.preventDefault();
+        dispatch(addTodo(input))
+        setInput('');
+    }
+
+    return (
+        <form onSubmit={addTodoHandler} className="space-x-3 mt-8">
+            <input
+                type="text"
+                className="bg-gray-800 rounded border border-gray-700 text-base outline-none focus:outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="Enter a Todo..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+            <button
+                type="submit"
+                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+                Add Todo
+            </button>
+        </form>
+    )
+}
+
+export default AddTodo;
